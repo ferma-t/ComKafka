3fbb0fc90519872db9b6516ecacb55ddce79a48c
Alien Roswell <roswellalien47@outlook.com>
2021-01-26 11:32:34 +0300

    init commit

---
+++ ComKafka/ComKafka/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Confluent.Kafka;
+using Kafka.Public;
+using Kafka.Public.Loggers;
+
+
+namespace ComKafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, collection) =>
+            {
+                collection.AddHostedService<KafkaConsumerHostedService>();
+                collection.AddHostedService<KafkaProducerHostedSerice>();
+
+            });
+    }
+
+    public class KafkaConsumerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaConsumerHostedService> _logger;
+        private readonly ClusterClient _cluster;
+
+        public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+        {
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration
+            {
+                Seeds = "vs-dc1-mb-ks01.net.berg.ru:9092"
+            }, new ConsoleLogger());
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cluster.ConsumeFromLatest("demo");
+            _cluster.MessageReceived += record =>
+            {
+                _logger.LogInformation($"Received: {Encoding.UTF8.GetString(record.Value as byte[])}");
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cluster?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+    public class KafkaProducerHostedSerice : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedSerice> _logger;
+        private readonly IProducer<Null, String> _producer;
+
+        public KafkaProducerHostedSerice(ILogger<KafkaProducerHostedSerice> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "vs-dc1-mb-ks01.net.berg.ru:9092",
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+
+
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var value = $"Hello world{i}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("demo", new Message<Null, string>()
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+            _producer.Flush(TimeSpan.FromSeconds(10));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer.Dispose();
+            return Task.CompletedTask;
+
+        }
+    }
+
+}
